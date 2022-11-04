13ec686589474d82c3261a8b70c89d1db2a85052
Ayse <aecer1@jh.edu>
2022-11-04 13:53:14 -0400

    week6

--- week6-homework/analysis/heatmap.py
+++ week6-homework/analysis/heatmap.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 
 import sys
-
 import numpy
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
 def filter(raw_data, start_bin, end_bin): 
     # Filter out data with one or both interaction ends falling outside the desired bin range. Filter raw data to be just the data you want to use which is 11170245-12070245(chr15) and save it in filter_data.
-    mask = numpy.where((raw_data['F1'] >= start_bin) & (raw_data['F2'] < end_bin))
+    mask = numpy.where((raw_data['F1'] > start_bin) & (raw_data['F2'] < end_bin))
     filter_data = raw_data[mask]
     # set the minimum F1 value to 0 and minimum F2 value to 0
     # minus the smallest value of F1 from all values of F1 (the smallest possibel is start_bin)
@@ -20,14 +19,9 @@ def filter(raw_data, start_bin, end_bin):
     
 def sparse_to_matrix(sparse, n): #n is the length of the genomic region (n = end_bin - start_bin)
     mat = numpy.zeros((n, n), dtype=float) # create mat (an empty matrix) 
-    #fill in the matrix use sparse. ~2 lines of code.
-    #mat[sparse['F1'][i], sparse['F2'][i]] = sparse['score'][i] # this is the line they gave you don't use this
     mat[sparse['F1'], sparse['F2']] = sparse['score'] #two inputs to find a quandrant and put score there.
     mat[sparse['F2'], sparse['F1']] = sparse['score'] #for mirror image
     return mat
-    
-# Plot the two matrices using the same maximum value (set vmax in imshow). 
-# I suggest using the magma color map, although you need to flip your scores to mimic the paper figure
 
 def remove_dd_bg(mat):
     N = mat.shape[0]
@@ -48,6 +42,7 @@ def smooth_matrix(mat):
             nmat += mat[i:(N - 2 + i), j:(N - 2 + j)]
     nmat /= 9
     nmat[invalid] = 0
+    print(nmat)
     return nmat
 
 def main():
@@ -92,8 +87,11 @@ def main():
     ax[0].axis('off')
     ax[1].imshow(-smooth_removed_matrix_data2, vmax=vmax, vmin=vmin, cmap='magma')
     ax[1].axis('off')
+    plt.colorbar()
     ax[2].imshow(matrix3, cmap='seismic', norm=colors.CenteredNorm())
     ax[2].axis('off')
+    plt.colorbar()
+    plt.tight_layout()
     plt.savefig(out_fname)
     
 main()
